6bf5bb7b79cffb3eea47566d14212e1a4dd95b5c
ichadharimanushya <parthlohani2008@gmail.com>
2026-07-17 22:37:18 +0530

    board.py is complete

--- CLIChess/board.py
+++ CLIChess/board.py
@@ -1,11 +1,6 @@
-
-
 from pieces import Pawn, Rook, Knight, Bishop, Queen, King
 
-
 # for chess board
-
-
 class Board():
     def __init__(self):
         self.board = [[Rook("black"), Knight("black"), Bishop("black"), Queen("black"), King("black"), Bishop("black"), Knight("black"), Rook("black")],
@@ -17,11 +12,7 @@ class Board():
                 [Pawn("white"), Pawn("white"), Pawn("white"), Pawn("white"), Pawn("white"), Pawn("white"), Pawn("white"), Pawn("white")],
                 [Rook("white"), Knight("white"), Bishop("white"), Queen("white"), King("white"), Bishop("white"), Knight("white"), Rook("white")],]
 
-    # def get_board(self):
-    #     return self.board
-
-    def print_board(self):
-        # for a better view of the board/showing the current board in the terminal
+    def print_board(self):  # this function is done, final, packed and sealed.
         number = 8
         for row in self.board:
             print("-----------------------------------------", end = "\n| ")
@@ -37,22 +28,27 @@ class Board():
             print(chr(ord('a')+i), end = "    ")
         print()
 
-    def get_piece(self, row, col):
-        # to get the piece at a position
+    def get_piece(self, row, col):  # this function is done, final, packed and sealed.
         return self.board[row][col]
 
-    def remove_piece(self, row, col):
-        # to remove a piece form a postition and return it
+    def remove_piece(self, row, col):   # this function is done, final, packed and sealed.
         piece = self.board[row][col]
         self.board[row][col] = None
         return piece
 
-    def set_piece(self, row, col, piece):
-        # to set a piece to a potition
+    def set_piece(self, row, col, piece):   # this function is done, final, packed and sealed.
         self.board[row][col] = piece
 
-    def is_valid_position(self, row, col):
+    def is_valid_position(self, row, col):      # this function is done, final, packed and sealed.
         return (0 <= row < 8 and 0 <= col < 8)
     
+    def get_pieces_of_color(self, color):   # this function is done, final, packed and sealed.
+        pieces = []
+        for row in range(8):
+            for col in range(8):
+                piece = self.board[row][col]
+                if piece is not None and piece.color == color:
+                    pieces.append((row, col, piece))
+        return pieces
     
 Board().print_board()
