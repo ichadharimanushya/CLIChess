739ea117c766d9e5d4602d59f25968313e59f968
ichadharimanushya <parthlohani2008@gmail.com>
2026-07-17 15:00:13 +0530

    Added board and coordinate sys

--- CLIChess/board.py
+++ CLIChess/board.py
@@ -1,1 +1,27 @@
 # for chess board
+
+def create_board():
+    board = [['♖', '♘', '♗', '♕','♔', '♗', '♘', '♖'],
+             ['♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'],
+             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+             ['♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟'],
+             ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'],]
+
+    # for any operations
+    return board
+
+def print_board(board):
+    # for a better view of the board/showing the current board in the terminal
+    number = 8
+    for row in board:
+        print("-----------------------------------------", end = "\n| ")
+        for piece in row:
+            print(f"{piece}", end = "  | ")
+        print(number, end = "\n")
+        number -= 1
+    print("-----------------------------------------\n  ", end = "")
+    for i in range(8):
+        print(chr(ord('a')+i), end = "    ")
--- CLIChess/move.py
+++ CLIChess/move.py
@@ -1,1 +1,23 @@
 # move representation (optional but useful)
+
+def coordinate_to_index(square = ""):
+    xy = square.strip().lower()
+
+    # base case
+    if len(xy) != 2: return None    
+    if not xy[0].isalpha(): return None
+    if not xy[1].isnumeric(): return None
+
+    # coordinate according to the index of board variables
+    # return (8-int(xy[1]), ord(xy[0])-ord('a'))
+    file = xy[0]
+    rank = int(xy[1])
+
+    if not (ord('a') <= ord(file) and ord(file) <= ord('h')):
+        return None
+    if not (1 <= rank and rank <= 8):
+        return None
+
+    row = 8 - rank
+    col = ord(file) - ord("a")
+    return row, col
